04a823903538033b2eb7714dbe64362ed51f042b
Mr Mystery 1.0 <69641472+MrMystery10-del@users.noreply.github.com>
2023-03-19 15:37:47 +0100

    Fixed a small resize bug
    Images of the players were not correctly resized

--- src/gui/inGameScreen/PlayersGUI.java
+++ src/gui/inGameScreen/PlayersGUI.java
@@ -18,12 +18,12 @@ public class PlayersGUI extends Screen {
         Font font = new Font("Comic Sans MS", Font.BOLD, 70);
 
         Label label1 = new Label(player1.getName(), Shapes.OVAL);
-        label1.setBounds(640, 0, 800, 200);
+        label1.setBounds(640, 540, 800, 200);
         label1.setFont(font);
         add(label1);
 
         Label label2 = new Label(player2.getName(), Shapes.OVAL);
-        label2.setBounds(640, 540, 800, 200);
+        label2.setBounds(640, 0, 800, 200);
         label2.setFont(font);
         add(label2);
     }
@@ -38,8 +38,8 @@ public class PlayersGUI extends Screen {
         g2d.fillRect(0, 0, getWidth(), getHeight());
 
         int imageSize = getHeight() / 2;
-        g2d.drawImage(player2.getImage(), 0, 0, imageSize, imageSize, null);
-        g2d.drawImage(player1.getImage(), 0, imageSize, imageSize, imageSize, null);
+        g2d.drawImage(player2.getImage(), 0, 0, (int) (getWidth() / 3.55), imageSize, null);
+        g2d.drawImage(player1.getImage(), 0, imageSize, (int) (getWidth() / 3.55), imageSize, null);
 
         g2d.setColor(Color.BLACK);
         g2d.drawLine(0, imageSize, getWidth(), imageSize);
