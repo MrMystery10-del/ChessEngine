58c8a219c54e598c1e8ee2273cbd6d39840d3120
Hiel Jorn <fa285634@skynet.be>
2023-03-19 20:53:57 +0100

    Internationalization started on menu's and start/load/save menu added

--- src/gui/InGameScreen.java
+++ src/gui/InGameScreen.java
@@ -3,21 +3,30 @@ package gui;
 import content.Screen;
 import core.pojo.Player;
 import gui.inGameScreen.Board;
+import gui.inGameScreen.GameStateGui;
 import gui.inGameScreen.PlayersGUI;
 import gui.inGameScreen.StateGUI;
 import manage.ImageManager;
 import manage.Pieces;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Main screen for playing the chess game, displays the board and pieces
  */
 public abstract class InGameScreen extends Screen {
 
+    //load current local language - english if not available (yet)
+    ResourceBundle resourceBundle = ResourceBundle.getBundle("gui.translations.bundle", Locale.getDefault());
+
     private Board board = new Board();
     private PlayersGUI playersGUI;
     private StateGUI stateGUI = new StateGUI();
     private Player player1 = new Player("some User", ImageManager.getPiece(Pieces.KING)); // Placeholder
     private Player player2 = new Player("some AI", ImageManager.getPiece(Pieces.KING_BLACK));; // Placeholder
+
+    private GameStateGui gameStateGui = new GameStateGui(resourceBundle);
 
     /**
      * Construct the screen
@@ -31,8 +40,14 @@ public abstract class InGameScreen extends Screen {
 
         stateGUI.setBounds(0, 800, 1920, 280);
 
+        gameStateGui.setBounds(800,800,1120,100);
+
+
+
         add(board);
         add(playersGUI);
+        add(gameStateGui);
         add(stateGUI);
+
     }
 }
--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -3,16 +3,23 @@ package gui;
 import content.Button;
 import content.Screen;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public abstract class MainMenu extends Screen {
 
     /** Set up a new Screen with buttons */
     public MainMenu() {
-        Button multiplayer = new Button("Multiplayer");
+
+        Locale locale_en_EN = new Locale("en", "EN");
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("gui.translations.bundle", locale_en_EN);
+
+        Button multiplayer = new Button(resourceBundle.getString("multiplayer"));
         multiplayer.setBounds(460, 200, 1000, 200);
         multiplayer.addActionListener(event -> startedMultiplayer());
         add(multiplayer);
 
-        Button ai = new Button("Against AI");
+        Button ai = new Button(resourceBundle.getString("vsAi"));
         ai.setBounds(460, 600, 1000, 200);
         ai.addActionListener(event -> startedAI());
         add(ai);
