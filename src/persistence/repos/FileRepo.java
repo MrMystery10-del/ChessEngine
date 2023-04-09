85466f8100134b0e945a48d3d626efeda749f93e
Hiel Jorn <fa285634@skynet.be>
2023-04-09 20:27:44 +0200

    Junit added, small reconfig to library instead of single item lib.

--- src/persistence/repos/FileRepo.java
+++ src/persistence/repos/FileRepo.java
@@ -1,5 +1,6 @@
 package persistence.repos;
 
+import core.Configuration;
 import persistence.DTO.ProfileDto;
 import persistence.interfaces.ProfileInterface;
 
@@ -12,15 +13,42 @@ public class FileRepo implements ProfileInterface {
 
     private static final Logger logger = Logger.getLogger(FileRepo.class.getName());
     private static FileRepo instance;
-    private FileRepo() {}
+    private String userConfigLocation;
+
+    //private constructor -> do NOT change access
+    private FileRepo() {
+        userConfigLocation=System.getProperty("user.home");
+        if(!Configuration.userConfigLocation.isEmpty()){
+            if(Configuration.userConfigLocation.contains("/")) {
+                userConfigLocation = Configuration.userConfigLocation;
+                logger.log(Level.INFO, "user configuration set to -> " + userConfigLocation);
+            }
+            else
+            {
+                logger.log(Level.SEVERE, "Invalid file path set");
+                throw new IllegalArgumentException("User configuration path is not valid");
+            }
+        }
+    }
 
     public static FileRepo getInstance() {
         if (instance == null) {
             instance = new FileRepo();
         }
         return instance;
+
     }
 
+    /**
+     * destroy the singleton ( use for testing )
+     */
+    public static void destroy(){
+        instance=null;
+    }
+    
+    
+
+    
 
     @Override
     public Supplier<ProfileDto> readFromProfile() {
---
+++ src/test/core/pojo/PiecesTest.java
@@ -0,0 +1,41 @@
+package core.pojo;
+
+import core.pojo.pieces.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PiecesTest {
+
+    private Piece bishop;
+    private Piece rook;
+    private Piece king;
+    private Piece knight;
+    private Piece pawn;
+    private Piece queen;
+
+
+    @BeforeEach
+    void setup() {
+        bishop = new Bishop();
+        rook = new Rook();
+        king = new King();
+        knight = new Knight();
+        pawn = new Pawn();
+        queen = new Queen();
+    }
+
+    @Test
+    void doesExist() {
+        assertNotNull(bishop);
+        assertNotNull(rook);
+        assertNotNull(king);
+        assertNotNull(knight);
+        assertNotNull(pawn);
+        assertNotNull(queen);
+    }
+
+}
+
+
