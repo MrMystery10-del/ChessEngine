593fbd9ddd4430abacc08d0f91441cf68ddc7c74
Shekhar Pegu <133399367+Shekharcodess@users.noreply.github.com>
2023-05-25 20:28:37 +0530

    Update Pieces.java #40 by shekharcodess
    I have added comments to provide a detailed explanation of each class, including the purpose of the class, the functionality it provides, and the usage of its methods to improve the documentation.

--- src/main/java/utils/Pieces.java
+++ src/main/java/utils/Pieces.java
@@ -1,5 +1,11 @@
 package utils;
 
+/**
+ * This enum represents the types of chess pieces in the game.
+ * It includes the standard pieces (PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING),
+ * their black counterparts (PAWN_BLACK, BISHOP_BLACK, KNIGHT_BLACK, ROOK_BLACK, QUEEN_BLACK, KING_BLACK),
+ * and an EMPTY value.
+ */
 public enum Pieces {
     PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING,
     PAWN_BLACK, BISHOP_BLACK, KNIGHT_BLACK, ROOK_BLACK, QUEEN_BLACK, KING_BLACK,
